04164a5502795bfd686879adbdf175b569a86578
張逸辛 <zhangyixin@chouitsushin-no-MacBook-Air.local>
2015-09-20 13:32:58 +0900

    fix error

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -12,7 +12,7 @@ class StaticPagesControllerTest < ActionController::TestCase
     assert_response :success
     assert_select "title", "Help | Ruby on Rails Tutorial Sample App"
   end
-  
+
   test "should get about" do
     get :about
     assert_response :success
