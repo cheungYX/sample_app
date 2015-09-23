8beb2fec75bc89c49a13f9590417705f79025281
張逸辛 <zhangyixin@chouitsushin-no-MacBook-Air.local>
2015-09-23 13:40:07 +0900

    第１１章

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-
   root             'static_pages#home'
   get 'help'    => 'static_pages#help'
   get 'about'   => 'static_pages#about'
@@ -11,4 +10,5 @@ Rails.application.routes.draw do
   resources :users
   resources :account_activations, only: [:edit]
   resources :password_resets,     only: [:new, :create, :edit, :update]
+  resources :microposts,          only: [:create, :destroy]
 end
